28a2845e1fd47a3940e6f512f00e754bab02b912
unknown <jorgemantinanmontero@gmail.com>
2026-02-28 16:36:39 +0100

    Added main route

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const axios = require('axios');
 const jwt = require('jsonwebtoken');
 
 const app = express();
-app.use(cors()); // Configura esto con tu dominio real en producción
+app.use(cors({
+    origin: 'https://jorgemantinan.github.io/manti-twitch/'
+}));
 app.use(express.json());
+
+// Main
+app.get('/', (req, res) => {
+  res.send('<h1>🚀 Tamoh ready 🚀</h1>');
+});
 
 // 1. Redirigir a Twitch (esto lo inicia el móvil)
 app.get('/auth/twitch', (req, res) => {
@@ -16,36 +23,30 @@ app.get('/auth/twitch', (req, res) => {
 
 // 2. Callback: Twitch nos da el código
 app.get('/auth/callback', async (req, res) => {
+    const { code } = req.query;
+
     try {
-        res.send('🚀 El servidor de Manti está vivo y operando.');
+        // Intercambiamos código por Token real
+        const response = await axios.post('https://id.twitch.tv/oauth2/token', null, {
+            params: {
+                client_id: process.env.TWITCH_CLIENT_ID,
+                client_secret: process.env.TWITCH_CLIENT_SECRET,
+                code,
+                grant_type: 'authorization_code',
+                redirect_uri: process.env.TWITCH_REDIRECT_URI
+            }
+        });
+
+        const twitchToken = response.data.access_token;
+
+        // Creamos nuestro propio JWT para el móvil
+        const userToken = jwt.sign({ twitchToken }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        // Redirigimos de vuelta a la App con el token (usando un Deep Link)
+        res.redirect(`tuapp://login?token=${userToken}`);
     } catch (error) {
-        res.status(res.error.status).send('Error en la autenticación');
+        res.status(500).send('Error en la autenticación');
     }
-    
-    // const { code } = req.query;
-
-    // try {
-    //     // Intercambiamos código por Token real
-    //     const response = await axios.post('https://id.twitch.tv/oauth2/token', null, {
-    //         params: {
-    //             client_id: process.env.TWITCH_CLIENT_ID,
-    //             client_secret: process.env.TWITCH_CLIENT_SECRET,
-    //             code,
-    //             grant_type: 'authorization_code',
-    //             redirect_uri: process.env.TWITCH_REDIRECT_URI
-    //         }
-    //     });
-
-    //     const twitchToken = response.data.access_token;
-
-    //     // Creamos nuestro propio JWT para el móvil
-    //     const userToken = jwt.sign({ twitchToken }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-    //     // Redirigimos de vuelta a la App con el token (usando un Deep Link)
-    //     res.redirect(`tuapp://login?token=${userToken}`);
-    // } catch (error) {
-    //     res.status(500).send('Error en la autenticación');
-    // }
 });
 
 // 3. Endpoint Protegido para obtener chatters
