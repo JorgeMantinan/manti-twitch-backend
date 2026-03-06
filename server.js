648979bb7083a2f607242899462e84ed7153812c
unknown <jorgemantinanmontero@gmail.com>
2026-03-06 17:59:39 +0100

    Hotfix .env PORT

--- server.js
+++ server.js
@@ -554,6 +554,6 @@ app.post('/api/raffle/pick-winner', (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(process.env.TWITCH_CLIENT_ID , () => {
+  console.log(`Server running on port ${process.env.TWITCH_CLIENT_ID }`);
 });
